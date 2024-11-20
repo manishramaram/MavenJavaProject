e90aaefd6671ee74621d30c09156057b23b3adca
manishramaram <manishramaram2@gmail.com>
2024-11-20 23:15:01 +0530

    Hello World

---
+++ src/main/java/SEGROUP/MavenJavaProject/App.java
@@ -0,0 +1,8 @@
+package SEGROUP.MavenJavaProject;
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "hi i am manish ramaram 22bd1a6654" );
+    }
+}
